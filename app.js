803a0a765383f34b9716ab4e6e95ced2bab07a99
elgatovoladorr <elgatovoladoryt@gmail.com>
2025-02-24 21:18:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//Cantida de noticias que se cargaran cada vez que se presione siguiente (5 + 1)
 let cantidadNoticias = 5;
 let pageFinal = cantidadNoticias;
 let pageInicial = 0;
@@ -6,77 +5,91 @@ let temaActual = "Tecnología";
 
 let noticias = {
     "apiKey":"f3798e116eb342b2bae58e7f0cbd9c11",
-    fetchNoticias:function(categoria){
+    fetchNoticias: function(categoria) {
         fetch(
             "https://newsapi.org/v2/everything?q="
-            +categoria+
-            "&languaje=es&apiKey="+this.apiKey
+            + categoria +
+            "&language=es&apiKey=" + this.apiKey
         )
-        .then((response)=>response.json())
-        .then((data)=>this.displayNoticias(data));
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Error al obtener los datos de la API: ' + response.status);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.articles && data.articles.length > 0) {
+                this.displayNoticias(data);
+            } else {
+                console.log("No se encontraron artículos para la categoría: " + categoria);
+                document.querySelector(".container-noticias").textContent = "No se encontraron artículos.";
+            }
+        })
+        .catch((error) => {
+            console.error("Hubo un problema con la solicitud:", error);
+            document.querySelector(".container-noticias").textContent = "Hubo un error al cargar las noticias.";
+        });
     },
-    displayNoticias: function(data){
-        //elimino todo si ha seleccionado un nuevo tema
-        if(pageInicial==0){
-            document.querySelector(".container-noticias").textContent ="";
+    
+    displayNoticias: function(data) {
+        // Elimino todo si se ha seleccionado un nuevo tema
+        if (pageInicial == 0) {
+            document.querySelector(".container-noticias").textContent = "";
         }
 
+        for (let i = pageInicial; i < pageFinal && i < data.articles.length; i++) {
+            const { title, urlToImage, publishedAt, source, url } = data.articles[i];
 
-        for(i=pageInicial;i<=pageFinal;i++){
-            const {title} = data.articles[i];
             let h2 = document.createElement("h2");
             h2.textContent = title;
-    
-            const {urlToImage} = data.articles[i];
+
             let img = document.createElement("img");
             img.setAttribute("src", urlToImage);
 
             let info_item = document.createElement("div");
             info_item.className = "info_item";
-            const {publishedAt} = data.articles[i];
+
             let fecha = document.createElement("span");
-            let date = publishedAt;
-            date=date.split("T")[0].split("-").reverse().join("-");
+            let date = publishedAt.split("T")[0].split("-").reverse().join("-");
             fecha.className = "fecha";
             fecha.textContent = date;
 
-            const {name} = data.articles[i].source;
             let fuente = document.createElement("span");
             fuente.className = "fuente";
-            fuente.textContent = name;
+            fuente.textContent = source.name;
 
             info_item.appendChild(fecha);
             info_item.appendChild(fuente);
-
-            const {url} = data.articles[i];
 
             let item = document.createElement("div");
             item.className = "item";
             item.appendChild(h2);
             item.appendChild(img);
             item.appendChild(info_item);
-            item.setAttribute("onclick", "location.href='"+url+"'");
+            item.setAttribute("onclick", "location.href='" + url + "'");
+
             document.querySelector(".container-noticias").appendChild(item);
         }
 
-        let btnSiguiente = document.createElement("span");
-        btnSiguiente.id = "btnSiguiente";
-        btnSiguiente.textContent = "Ver más";
-        btnSiguiente.setAttribute("onclick","siguiente()");
-        document.querySelector(".container-noticias").appendChild(btnSiguiente);
+        // Mostrar botón "Ver más" solo si hay más noticias para cargar
+        if (pageFinal < data.articles.length) {
+            let btnSiguiente = document.createElement("span");
+            btnSiguiente.id = "btnSiguiente";
+            btnSiguiente.textContent = "Ver más";
+            btnSiguiente.setAttribute("onclick", "siguiente()");
+            document.querySelector(".container-noticias").appendChild(btnSiguiente);
+        }
     }
-}
+};
 
-
-
-function buscar(cat){
+function buscar(cat) {
     pageInicial = 0;
     pageFinal = cantidadNoticias;
     temaActual = cat;
     noticias.fetchNoticias(cat);
 }
 
-function buscarTema(){
+function buscarTema() {
     pageInicial = 0;
     pageFinal = cantidadNoticias;
 
@@ -85,13 +98,14 @@ function buscarTema(){
     noticias.fetchNoticias(temaActual);
 }
 
-function siguiente(){
-    pageInicial = pageFinal + 1;
-    pageFinal = pageFinal + cantidadNoticias + 1;
-    //eliminamos el botón siguiente
+function siguiente() {
+    pageInicial = pageFinal;
+    pageFinal = pageFinal + cantidadNoticias;
+
+    // Eliminamos el botón siguiente
     document.querySelector("#btnSiguiente").remove();
     noticias.fetchNoticias(temaActual);
-
 }
 
+// Cargar las noticias iniciales
 noticias.fetchNoticias(temaActual);
